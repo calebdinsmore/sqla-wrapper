10c30ba490fb10141a8faac451cd0f8a4b3555b0
Caleb Dinsmore <calebjdinsmore@gmail.com>
2021-04-15 13:23:12 -0400

    Rewrote create_or_first to not cause persistence warning to trigger

--- sqla_wrapper/default_model.py
+++ sqla_wrapper/default_model.py
@@ -22,11 +22,11 @@ def get_default_model_class(db):
         def create_or_first(cls, **attrs):
             """Tries to create a new record, and if it fails
             because already exists, return the first it founds."""
-            try:
+            first = cls.first(**attrs)
+            if first:
+                return first
+            else:
                 return cls.create(**attrs)
-            except IntegrityError:
-                db.session.rollback()
-                return cls.first(**attrs)
 
         @classmethod
         def first(cls, **attrs):
